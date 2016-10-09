e27048f33ce66846b01f0fafdd842b6ac768e410
apatil88 <amrut.patil88@hotmail.com>
2016-10-09 15:46:11 -0400

    Added functionality to delete Todos

--- TodoListProject/assets/js/todos.js
+++ TodoListProject/assets/js/todos.js
@@ -3,3 +3,16 @@
 $("li").click(function(){
    $(this).toggleClass("completed");
 });
+
+//Delete Todos
+$("span").click(function(event){
+   
+    //($this) here refers to the span
+    //$(this).parent() returns the enclosing li
+    $(this).parent().fadeOut(500, function(){
+        //$(this) here refers to the parent of the span i.e. li
+       $(this).remove(); 
+    });
+    //Prevent event bubbling i.e. propagation of events to the li, container div and body
+    event.stopPropagation();
+});
